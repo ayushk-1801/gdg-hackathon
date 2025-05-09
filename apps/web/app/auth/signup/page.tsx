c8f5df290598e6be2990136cf9d9f1b83b2815ca
Ayush Kumar <ayush23162@iiitd.ac.in>
2025-05-09 20:32:25 +0530

    Add Google sign-in functionality to the signup page; remove redundant onClick handler from SVG icon

--- apps/web/app/auth/signup/page.tsx
+++ apps/web/app/auth/signup/page.tsx
@@ -253,6 +253,7 @@ function SignUp() {
                 type="button"
                 variant="outline" 
                 className="w-full py-3 flex items-center justify-center space-x-2"
+                onClick={handleGoogleSignIn}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -263,7 +264,6 @@ function SignUp() {
                   fillRule="evenodd"
                   clipRule="evenodd"
                   className="w-5 h-5"
-                  onClick={handleGoogleSignIn}
                 >
                   <path
                     d="M326667 170370c0-13704-1112-23704-3518-34074H166667v61851h91851c-1851 15371-11851 38519-34074 54074l-311 2071 49476 38329 3428 342c31481-29074 49630-71852 49630-122593m0 0z"
